dd60e6144eab1245e3c48fb0b7a561ea48bcfa7d
wilson <wil82-xbox@hotmail.com>
2024-10-22 10:47:20 -0500

    subir cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './estructura/principal.component';
+import { DashboardComponent } from './modulos/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: PrincipalComponent,
+        children: [
+          {
+            path: 'dashboard',
+            component: DashboardComponent,
+          },
+          {
+            path: '',
+            redirectTo: 'dashboard',
+            pathMatch: 'full',
+          }
+        ]
+      }
+];
