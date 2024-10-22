69ecc2dd3b115f55736d7971febae87dccd7c51e
wilson <wil82-xbox@hotmail.com>
2024-10-22 13:38:55 -0500

    actualizar repositorio

--- src/app/estructura/principal.component.ts
+++ src/app/estructura/principal.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { NavComponent } from "./nav/nav.component";
+import { MenuComponent } from "./menu/menu.component";
+import { FooterComponent } from "./footer/footer.component";
+import { AppComponent } from "../app.component";
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [NavComponent, MenuComponent, FooterComponent, AppComponent],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
